a01bd802e23006339483f398a746b1069feeadf6
Dementy Sivakov <dsivakov@yandex.ru>
2025-12-04 13:24:07 +0300

    Создан класс UserStep(с шагами пользователя)

---
+++ src/main/java/ru/yandex/practicum/user/UserStep.java
@@ -0,0 +1,59 @@
+package ru.yandex.practicum.user;
+
+import io.qameta.allure.Step;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+
+import static io.restassured.RestAssured.given;
+import static ru.yandex.practicum.utils.Constants.*;
+
+
+public class UserStep {
+
+    @Step("Создание пользователя")
+    public ValidatableResponse createUser(User user) {
+        return given()
+                .contentType(ContentType.JSON)
+                .body(user)
+                .when()
+                .post(POST_REGISTER)
+                .then().statusCode(200);
+    }
+
+    @Step("Удаление пользователя")
+    public ValidatableResponse deleteUser(User user) {
+        System.out.println("Токен для удаления: " + user.getAccessToken()); // для проверки
+        return given()
+                .spec(getSpec("Bearer " + user.getAccessToken()))
+                .log().all() // логируем запрос
+                .when()
+                .delete(DELET_USER)
+                .then()
+                .log().all(); // логируем ответ
+    }
+
+    // Дополнительный метод для извлечения токена из ответа
+    public String extractAccessToken(ValidatableResponse response) {
+        return response.extract().body().jsonPath().getString("accessToken");
+    }
+
+    protected static RequestSpecification getSpec() {
+        return new RequestSpecBuilder()
+                .setContentType(ContentType.JSON)
+                .setBaseUri(HOST)
+                .build();
+    }
+
+    protected static RequestSpecification getSpec(String bearerToken) {
+        return new RequestSpecBuilder()
+                .setContentType(ContentType.JSON)
+                .addHeader("Authorization", bearerToken)
+                .setBaseUri(HOST)
+                .build();
+    }
+
+}
+
