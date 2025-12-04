19f8de6a6d41f5c7f13dfccf69b7aa92e02ffaa0
Dementy Sivakov <dsivakov@yandex.ru>
2025-12-04 13:56:13 +0300

    add allure report

--- src/main/java/ru/yandex/practicum/util/EnvConfig.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.util;
-
-public class EnvConfig {
-    public static String BASE_URL="https://stellarburgers.education-services.ru/";
-    public static final int IMPLICITY_TIMEOUT=10;
-    public static final int EXPLICITY_TIMEOUT=15;
-}
