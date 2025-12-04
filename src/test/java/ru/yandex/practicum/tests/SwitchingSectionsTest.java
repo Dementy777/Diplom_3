0ec43f55f3a2c046953fdf96e17dec88bd809d4f
Dementy Sivakov <dsivakov@yandex.ru>
2025-12-04 13:39:23 +0300

    добавлена проверка: раздел «Булки» активен при загрузке страницы

--- src/test/java/ru/yandex/practicum/tests/SwitchingSectionsTest.java
+++ src/test/java/ru/yandex/practicum/tests/SwitchingSectionsTest.java
@@ -1,4 +1,24 @@
 package ru.yandex.practicum.tests;
 
-public class SwitchingSectionsTest {
+import io.qameta.allure.Description;
+import io.qameta.allure.junit4.DisplayName;
+
+import ru.yandex.practicum.pageObject.HomePage;
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SwitchingSectionsTest extends BaseTest {
+    HomePage homePage;
+
+    @Test
+    @DisplayName("Проверка: раздел «Булки» активен при загрузке страницы")
+    @Description("При открытии главной страницы раздел «Булки» должен быть выбран по умолчанию. Проверяется наличие активного CSS-класса у вкладки.")
+    public void bunsSectionIsActiveOnLoad() {
+        homePage = new HomePage(driver);
+        homePage.waitForEnterAccountButton();
+
+        Assert.assertThat(homePage.getClassNameBuns(), CoreMatchers.containsString("tab_tab_type_current__2BEPc"));
+    }
+
 }
