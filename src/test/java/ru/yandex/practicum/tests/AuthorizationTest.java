0f92ae7bd68b8715057a3f2e05a92992e6447ba1
Dementy Sivakov <dsivakov@yandex.ru>
2025-12-04 13:35:40 +0300

    добавлен позитивный тест на вход по кнопке «Личный кабинет»

--- src/test/java/ru/yandex/practicum/tests/AuthorizationTest.java
+++ src/test/java/ru/yandex/practicum/tests/AuthorizationTest.java
@@ -63,6 +63,24 @@ public class AuthorizationTest extends BaseTest {
                 user.getEmail().toLowerCase(), profilePage.getEmailText().toLowerCase());
     }
 
+    @Test
+    @DisplayName("позитивный тест на вход по кнопке «Личный кабинет»")
+    @Description("Позитивный тест на вход через регистрацию по API, логина пользователя и проверки по email")
+    public void enterProfileButtonTest() {
+        homePage.waitForPersonalAccountButton();
+        homePage.enterPersonalAccountButton();
+        authorizationPage = new AuthorizationPage(driver);
+        authorizationPage.waitForPageLoad();
+        authorizationPage.userDataEntry(user.getEmail(), user.getPassword());
+        authorizationPage.clickEnterButton();
+        homePage.waitCheckoutButton();
+        homePage.enterPersonalAccountButton();
+        profilePage = new ProfilePage(driver);
+        profilePage.waitProfilePageLoad();
+        assertEquals("Email в профиле не совпадает с зарегистрированным (игнорируем регистр)",
+                user.getEmail().toLowerCase(), profilePage.getEmailText().toLowerCase());
+    }
+
 
 
     @After
